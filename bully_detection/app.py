eabb15669bdcfa4dc33e8f04856cd0d916b1c89a
Vaishnav N T <68477941+Vaishnav-NT@users.noreply.github.com>
2021-06-10 00:25:48 +0530

    Integrate neural network with flask

--- bully_detection/app.py
+++ bully_detection/app.py
@@ -2,13 +2,27 @@ from flask import Flask,render_template,url_for,request
 import numpy as np
 import pickle
 import pandas as pd
+import tensorflow as tf
+import math
+import os
+import joblib
+
+from keras.models import load_model
+loaded_model = load_model('network.h5')
+Tokeniser = tf.keras.preprocessing.text.Tokenizer()
+
+def Predict(seed_text):
+    seed_text = Tokeniser.texts_to_sequences(seed_text)
+    seed_text = tf.keras.preprocessing.sequence.pad_sequences(seed_text, 342)
+    prediction = loaded_model(seed_text)
+    print(str(prediction))
 
 app=Flask(__name__)
 
 @app.route('/',methods=['PUT',"GET","POST"])
 def home():
     id=request.form.get("userid")
-    print(id)
+    #Predict(id)
     return render_template('index.html')
 
 
