ade061f7ef928ff162bc9c6585dcf3477137df39
Vaishnav N T <68477941+Vaishnav-NT@users.noreply.github.com>
2021-06-10 10:47:52 +0530

    model is working

--- bully_detection/app.py
+++ bully_detection/app.py
@@ -6,24 +6,53 @@ import tensorflow as tf
 import math
 import os
 import joblib
+import keras
+
+
+#from keras import model_from_json
+#json_file = open('model.json','r') 
+#loaded_model_json = json_file.read() 
+#json_file.close() 
+
+# use Keras model_from_json to make a loaded model 
+#loaded_model = model_from_json(loaded_model_json) 
+
+# load weights into new model 
+#loaded_model.load_weights("model.h5") 
+#print("Loaded Model from disk") 
+
+# compile and evaluate loaded model 
+#loaded_model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+
 
 from keras.models import load_model
 loaded_model = load_model('network.h5')
-Tokeniser = tf.keras.preprocessing.text.Tokenizer()
 
 def Predict(seed_text):
-    seed_text = Tokeniser.texts_to_sequences(seed_text)
-    seed_text = tf.keras.preprocessing.sequence.pad_sequences(seed_text, 342)
-    prediction = loaded_model(seed_text)
+    print('About to predict')
+    #seed_text = Tokeniser.texts_to_sequences(seed_text)
+    #seed_text = tf.keras.preprocessing.sequence.pad_sequences(seed_text, 342)
+
+    x = [seed_text]
+    Tokeniser = tf.keras.preprocessing.text.Tokenizer()
+    Tokeniser.fit_on_texts(x)
+    x = Tokeniser.texts_to_sequences(x)
+    x = tf.keras.preprocessing.sequence.pad_sequences(x, 342)
+
+    x = np.array(x)
+    #print(x)
+    
+    prediction = loaded_model(x)
     print(str(prediction))
+    return str(prediction)
 
 app=Flask(__name__)
 
 @app.route('/',methods=['PUT',"GET","POST"])
 def home():
     id=request.form.get("userid")
-    #Predict(id)
-    return render_template('index.html')
+    ans = Predict(id)
+    return render_template('index.html', ans=[id, ans])
 
 
 if __name__=="__main__":
